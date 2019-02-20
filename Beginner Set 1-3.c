5500db79ec2e20337a76d8688d166f98508a72c3
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-02-20 16:22:29 +0530

    Create Beginner Set 1-3.c

---
+++ Beginner Set 1-3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main() {
+   char ch;
+   printf("Enter an character :");
+   scanf("%c",&ch);
+      if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+       {
+           printf("vowel");
+       }
+       else
+       {
+           printf("consonent");
+       }
+       } 
