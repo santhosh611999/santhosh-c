cf1fc060c8e9968180a75285ad2fbe67e2855bb6
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-02-21 13:34:32 +0530

    Create Beginner Set 2-20.c

---
+++ Beginner Set 2-20.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+int num,nums;
+printf("Enter an num :");
+scanf("%d",&num);
+for(int i=1;i<=5;i++)
+{
+  nums=i*num;
+  printf("multiples are %d \n",nums);
+}
+}
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
