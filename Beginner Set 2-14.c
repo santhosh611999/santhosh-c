5a3f09b1e66bbdfffb6c2de5a16194f9a476e43b
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-02-21 12:42:18 +0530

    Create Beginner Set 2-14.c

---
+++ Beginner Set 2-14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int fnum,snum;
+printf("Enter an fnum :");
+scanf("%d",&fnum);
+printf("Enter an snum :");
+scanf("%d",&snum);
+for(int i=fnum;i<=snum;i++)
+{
+    if(i%2!=0)
+    {
+        printf("Odd numbers are %d \n",i);
+    }
+}
+}
+   
