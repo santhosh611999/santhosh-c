b68785bc79f7b4d1e8552b0a673975b065085dae
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-02-21 10:36:33 +0530

    Create Beginner Set 1-9.c

---
+++ Beginner Set 1-9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define MAX_SIZE 100
+void main()
+{
+	int n,k,m=0;
+	int a[MAX_SIZE];
+	printf("enter an num : ");
+	scanf("%d%d",&n,&k);
+	for(int i=0;i<n;i++)
+	{
+	    printf("Enter array values");
+	    scanf("%d",&a[i]);
+	}
+	for(int j=0;j<a[k];j++)
+	{
+	    m=a[j]+m;
+	}
+	 printf("value is %d",m);
+}
+	
