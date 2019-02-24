20b71059e9c82547c8fb37fb6a5f53691e352553
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-02-24 12:09:46 +0530

    Create Beginner Set 5-44.c

---
+++ Beginner Set 5-44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main() 
+{
+int num;
+printf("Enter  number:");
+scanf("%d",&num);
+if(num>=1&&num<=10)
+{
+    printf("Yes");
+}
+else
+{
+    printf("NO");
+}
+}
