b44111e175bf3ba008497c5bbf7c79c01d2840b9
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-02-26 02:38:09 -0800

    Create Beginner Set 7-68.c

---
+++ Beginner Set 7-68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int num,p;
+    printf("Enter the num");
+    scanf("%d",&num);
+   if(num%7==0)
+        {
+            printf("YES");
+        }
+        else
+        {
+         printf("NO");
+        }
+}
