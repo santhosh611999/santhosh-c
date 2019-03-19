3737988f1ec651b4a78e868ea9acbe193f3a623c
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-03-19 15:30:47 +0530

    Create Beginner Set 8-8.c

---
+++ Beginner Set 8-8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main() 
+{
+    int n,i;
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes...%d is a multiple of 13",n);
+    }
+    else
+    {
+    printf("no");
+    }
+}
