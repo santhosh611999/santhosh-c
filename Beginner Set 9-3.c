856074b9ff4bfe96558a769dfd8fb9e261e692da
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-03-19 15:41:47 +0530

    Create Beginner Set 9-3.c

---
+++ Beginner Set 9-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main() 
+{	
+   int n1,n2,i;
+   char c;
+   for(i=0;i<4;i++)
+   {
+       scanf("%d %c %d",&n1,&c,&n2);
+       if(c=='/')
+       {
+           printf("%d\n",(n1/n2));
+       }
+       else
+       {
+           printf("%d\n",(n1%n2));
+       }
+   }
+}
