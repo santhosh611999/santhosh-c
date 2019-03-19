fc2d31fcb54be4a46ec2f6c8ede7d27c65269300
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-03-19 15:33:42 +0530

    Create Beginner Set 8-10.c

---
+++ Beginner Set 8-10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main() 
+{
+    int n,rev=0,temp=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        temp=n%10;
+        rev=(rev*10)+temp;
+        n=n/10;
+    }
+    n=rev;
+    rev=0;
+    temp=0;
+    while(n!=0)
+    {
+        temp=n%10;
+        if(temp%2==1)
+        {
+           printf("%d ",temp); 
+        }
+        rev=(rev*10)+temp;
+        n=n/10;
+    }
+}
