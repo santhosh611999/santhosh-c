ccdbb7d209def9d9831cdf1bac80dad4a7b855d0
santhosh611999 <36331172+santhosh611999@users.noreply.github.com>
2019-03-19 15:25:45 +0530

    Create Beginner Set 8-5.c

---
+++ Beginner Set 8-5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+    char a[100];
+    int n,i,temp;
+    scanf("%s",a);
+    n=strlen(a);
+    if(n%2!=0)
+      {
+          temp=n/2;
+          a[temp]='*';
+          printf("%s",a);
+      }
+      else
+      {
+          temp=n/2;
+          a[temp]='*';
+          a[temp-1]='*';
+          printf("%s",a);
+      }
+}
